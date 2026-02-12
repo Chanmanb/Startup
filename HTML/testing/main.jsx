3c940ec60f149f29cde69e83274013f56a5af739
Chanman <chanmanb@byu.edu>
2026-02-12 12:49:14 -0700

    Getting a basic React program

---
+++ HTML/testing/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+    return (
+        <span>Help me</span>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
