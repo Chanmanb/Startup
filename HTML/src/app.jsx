6fe862985d7755373966116bd14dcbdc9319bce7
Chanman <chanmanb@byu.edu>
2026-02-12 18:40:46 -0700

    Simple Routing complete, though each app is essentially empty

--- HTML/src/app.jsx
+++ HTML/src/app.jsx
@@ -2,20 +2,33 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { Login } from './login/login';
+import { Friends } from './friends/friends';
+import { Feed } from './feed/feed';
+import { About } from './about/about';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+
 export default function App() {
-  return <div><header>
+  return <BrowserRouter>
+  <div><header>
             <div><h1 id="Site-name">PesterPage</h1></div>
             <nav id="navigator-nav">
                 <div id="navigator">
-                    <div className="nav-option"><a href="index.html">Home</a></div>
-                    <div className="nav-option"><a href="feed.html">Feed</a></div>
-                    <div className="nav-option"><a href="friends.html">Freinds</a></div>
-                    <div className="nav-option"><a href="about.html">About</a></div>
+                    <div className="nav-option"><NavLink to="login">Home</NavLink></div>
+                    <div className="nav-option"><NavLink to="feed">Feed</NavLink></div>
+                    <div className="nav-option"><NavLink to="friends">Freinds</NavLink></div>
+                    <div className="nav-option"><NavLink to="about">About</NavLink></div>
                 </div>
             </nav>
         </header>
 
-        <main>This is where appdata goes</main>
+        <Routes>
+            <Route path='/' element={<Login />} exact />
+            <Route path='/Feed' element={<Feed />} exact />
+            <Route path='/Friends' element={<Friends />} exact />
+            <Route path='/About' element={<About />} exact />
+            <Route path='*' element={<NotFound />} exact />
+        </Routes>
 
         <footer>
             <span id="sellout">
@@ -24,5 +37,10 @@ export default function App() {
                 <div className="sellout-section" id="git"><a href="https://github.com/Chanmanb?tab=repositories" />Github account (please subscribe)</div>
                 </div>
             </span>
-        </footer></div>;
+        </footer></div>
+</BrowserRouter>;
+}
+
+function NotFound () {
+    return <main>404: Return to sender; Address not found D:</main>;
 }
