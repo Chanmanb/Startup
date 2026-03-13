7acb02b094427b71fa01bc46a99db6786c57bdbe
Chanman <chanmanb@byu.edu>
2026-03-12 22:56:47 -0600

    User creation post done and mininmal work on creation function

--- HTML/service/index.js
+++ HTML/service/index.js
@@ -19,6 +19,28 @@ app.use(cookieParser());
 //  sets up the public directory
 app.use(express.static("public"));
 
+// gets /api prepended to each exRouter address.
+var exRouter = express.Router();
+app.use(`/api`, exRouter);
+
+//account creation
+exRouter.post('auth/create', async (req, res) => {
+    if (await search_user("user_name", req.body.user_name)) {
+        res.status(409).send({msg: "User already exists"})
+    }
+    else {
+        const user = await create_user(req.body.user_name, req.body.password);
+        setAuthCookie(res, user.token);
+        res.send({user_name: user.user_name});
+    }
+});
+
+function create_user(user_name, password) {
+    const hashed = await bcrypt.hash(password, 10);
+    
+}
+
+
 
 // listening to port 3000
 app.listen(port, () => {
