cdffd38fd74c1516fbd912ec119604a4151c8205
Chanman <chanmanb@byu.edu>
2026-03-12 22:32:09 -0600

    got basic tools in index.jsx

---
+++ HTML/service/index.js
@@ -0,0 +1,12 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
+// Locations on the PesterPage are in temporary storage
+let users = [];
+let locations = {};
+
